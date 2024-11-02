c123b74352692bcec763c0f4c4cdd46d6f2fc36c
Ssionn <casper.kizewski.developer@gmail.com>
2024-11-02 12:33:51 +0100

    Format fixes

--- resources/views/components/team-avatar.blade.php
+++ resources/views/components/team-avatar.blade.php
@@ -1,3 +1,4 @@
 @props(['team'])
 
-<img src="{{ $team->getFirstMediaUrl('avatars') }}" alt="{{ $team->name }}" class="h-12 w-12 rounded-full object-cover" />
+<img src="{{ $team->getFirstMediaUrl('avatars') }}" alt="{{ $team->name }}"
+    class="h-12 w-12 rounded-full object-cover" />
