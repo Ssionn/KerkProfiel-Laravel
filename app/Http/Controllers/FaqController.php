c15aab4ee11b0179fdce8b82f2abd6518b807d10
Ssionn <casper.kizewski.developer@gmail.com>
2024-11-25 15:28:48 +0100

    Better way of faq fetching

--- app/Http/Controllers/FaqController.php
+++ app/Http/Controllers/FaqController.php
@@ -2,14 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Models\Faq;
-
 class FaqController extends Controller
 {
     public function index()
     {
-        $faqs = Faq::where('role_id', auth()->user()->role_id)->get();
+        $faqs = auth()->user()->role->faqs()->get();
 
         return view('faq.index', compact('faqs'));
     }
