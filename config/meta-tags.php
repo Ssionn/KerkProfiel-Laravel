8d72a639a9c7faa6ec78753515dbfb6e72ccf09c
Ssionn <casper.kizewski.developer@gmail.com>
2025-01-30 10:52:43 +0100

    meta tags

--- config/meta-tags.php
+++ config/meta-tags.php
@@ -3,4 +3,6 @@
 return [
     'title' => 'Bedieningsprofiel',
     'description' => 'Bedieningsprofiel is een website waar je surveys kunt aanmaken en de resultaten kunt bekijken van een bepaald persoon. Er zijn vijf personaliteiten die kunnen matchen.',
+    'author' => 'Bedieningenprofiel',
+    'copyright' => 'Bedieningenprofiel',
 ];
